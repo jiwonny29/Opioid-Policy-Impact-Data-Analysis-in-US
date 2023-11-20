f9b4113fc45e3fc5e53ace91917870fbf1f1cb85
Rafael Davila-Bugarin <rafaeldavila.b@gmail.com>
2023-11-20 11:11:09 -0500

    uncompressing and collapsing

--- 10_code/20_load_data.py
+++ 10_code/20_load_data.py
@@ -1,18 +1,39 @@
 import pandas as pd
 import numpy as np
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import date_format, sum, count
+
 
 # Create a SparkSession
 spark = SparkSession.builder.getOrCreate()
 
-# Define the file path
-file_path = "https://gfx-data.news-engineering.aws.wapo.pub/ne-static/arcos/v2/bulk/arcos_all_washpost.zip"  # Change to your file path
+
+# As the file was to large I unziped it and saved it in my local machine
+file_path = "/Users/rafaeldavila/Documents/Duke/Sem1/720_Practice_DS/opioid/arcos_all_washpost.tsv"
 
 # Read the data
-df = spark.read.csv(file_path, sep="\t", header=True, compression="zip")
+df = spark.read.csv(file_path, sep="\t", header=True)
 
-# List of states you want to filter
-# states_to_filter = ["FL", "TX", "WA"]
+# Define the Parquet file path
+# parquet_file_path = (
+#    "../00_source_data/arcos_all_washpost.parquet"  # Change to your Parquet file path
+# )
 
-# Filter the DataFrame
-# filtered_df = df.filter(df["BUYER_STATE"].isin(states_to_filter))
+# Save as a Parquet file
+# df.write.mode("ingnore").parquet(parquet_file_path)
+
+# Creating a new date variable for grouping
+df = df.withColumn("year_month", date_format("TRANSACTION_DATE", "yyyy-MM"))
+
+# Groupby the data
+
+df = df.groupBy("BUYER_STATE", "BUYER_COUNTY", "DRUG_NAME", "year_month").agg(
+    sum("MME").alias("total_morphine_mg"),
+    count("REPORTER_DEA_NO").alias("total_transactions"),
+)
+
+# Converting to pandas
+df_pd = df.toPandas()
+
+# Saving df_pf to parquet
+df_pd.to_parquet("../00_source_data/arcos_all_washpost_collapsed.parquet")
