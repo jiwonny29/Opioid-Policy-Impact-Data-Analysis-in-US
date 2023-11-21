9699f37e4093f62a30c65fc0f8b1d22bce81fc8f
Rafael Davila-Bugarin <rafaeldavila.b@gmail.com>
2023-11-21 15:10:11 -0500

    I made the changes proposed by Barbara

--- 10_code/20_load_data.py
+++ 10_code/20_load_data.py
@@ -9,10 +9,10 @@ spark = SparkSession.builder.getOrCreate()
 
 
 # As the file was to large I unziped it and saved it in my local machine
-file_path = "/Users/rafaeldavila/Documents/Duke/Sem1/720_Practice_DS/opioid/arcos_all_washpost.tsv"
+file_path = "../../arcos_all_washpost.tsv"
 
 # Read the data
-df = spark.read.csv(file_path, sep="\t", header=True)
+opioid_df = spark.read.csv(file_path, sep="\t", header=True)
 
 # Define the Parquet file path
 # parquet_file_path = (
@@ -23,17 +23,22 @@ df = spark.read.csv(file_path, sep="\t", header=True)
 # df.write.mode("ingnore").parquet(parquet_file_path)
 
 # Creating a new date variable for grouping
-df = df.withColumn("year_month", date_format("TRANSACTION_DATE", "yyyy-MM"))
+opioid_df = opioid_df.withColumn(
+    "year_month", date_format("TRANSACTION_DATE", "yyyy-MM")
+)
 
 # Groupby the data
-
-df = df.groupBy("BUYER_STATE", "BUYER_COUNTY", "DRUG_NAME", "year_month").agg(
+opioid_df = opioid_df.groupBy(
+    "BUYER_STATE", "BUYER_COUNTY", "DRUG_NAME", "year_month"
+).agg(
     sum("MME").alias("total_morphine_mg"),
     count("REPORTER_DEA_NO").alias("total_transactions"),
 )
 
 # Converting to pandas
-df_pd = df.toPandas()
+dataframe_to_print = opioid_df.toPandas()
 
 # Saving df_pf to parquet
-df_pd.to_parquet("../00_source_data/arcos_all_washpost_collapsed.parquet")
+dataframe_to_print.to_parquet(
+    "../20_intermediate_files/arcos_all_washpost_collapsed.parquet"
+)
--- 10_code/50_analyze_data.py
+++ 10_code/50_analyze_data.py
@@ -1,1 +1,12 @@
+"""
+In this code the propensity score matching is done to find counterfactuals for the treatment group.
+"""
 
+import pandas as pd
+import numpy as np
+
+
+# Importing data about chronic diseases
+chronic_disease_df = pd.read_csv(
+    "https://data.cdc.gov/api/views/g4ie-h725/rows.csv?accessType=DOWNLOAD"
+)
