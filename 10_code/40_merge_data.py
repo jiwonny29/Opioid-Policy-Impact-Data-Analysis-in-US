6952aa4c3ab210b7fff6e0139e06b029a798d6e3
Rafael Davila-Bugarin <rafaeldavila.b@gmail.com>
2023-11-22 15:42:37 -0500

    merging socioeco and finding counterfactuals with euclidian distance

--- 10_code/40_merge_data.py
+++ 10_code/40_merge_data.py
@@ -1,2 +1,124 @@
+import pandas as pd
+import numpy as np
 
-#test 
+# Importing opiods data
+opioids = pd.read_parquet(
+    "../20_intermediate_files/arcos_all_washpost_collapsed.parquet"
+)
+
+# We need to group it by year and state
+
+# First we need to create a year column
+opioids["year"] = [int(i[:4]) for i in opioids["year_month"]]
+
+# Dropping drugname and counties column
+opioids = opioids.drop(columns=["DRUG_NAME", "BUYER_COUNTY", "year_month"])
+
+# Grouping by year and state
+opioids = opioids.groupby(["BUYER_STATE", "year"]).sum().reset_index()
+
+# I found that in the opioids we have 57 unique states, which is weird because there are only 50 states in the US
+
+# I'm going to check which are the unique states
+states = pd.read_csv(
+    "https://github.com/jasonong/List-of-US-States/raw/master/states.csv"
+)
+
+states_opioids = opioids[["BUYER_STATE"]].copy()
+# Keep uniques
+states_opioids.drop_duplicates(inplace=True)
+# reset index
+states_opioids.reset_index(drop=True, inplace=True)
+# merge opioids and fips
+states_opioids = states_opioids.merge(
+    states, how="left", left_on="BUYER_STATE", right_on="Abbreviation", indicator=True
+)
+
+# If we look at merge "onlyleft" we can find: ARMED FORCES, Gunea, Northern Mariana Islands, Puerto Rico, Palau and Virgin Islands
+# we can drop them and drop Alaska and Hawaii
+
+uncommon_states = list(
+    states_opioids[states_opioids["_merge"] == "left_only"]["BUYER_STATE"]
+)
+
+# Hawaii and Alaska
+uncommon_states.extend(["AK", "HI"])
+
+# Dropping them
+opioids.drop(opioids[opioids["BUYER_STATE"].isin(uncommon_states)].index, inplace=True)
+
+# importing population data
+population = pd.read_csv(
+    "../00_source_data/Additional_datasets/population_by_age_and_sex.csv"
+)
+# renaming columns of interest
+population = population.rename(
+    columns={
+        "Total Population": "tot_pop",
+        "Statefips": "state_fips",
+        "Year": "year",
+        "Population 45-64": "p_greater45",
+    }
+)
+# Keep just the variables of interest
+population = population["state_fips year tot_pop p_greater45".split()]
+# Grouping by year and state
+population = population.groupby(["state_fips", "year"], as_index=False).sum()
+# Importing fips for states
+state_fips = pd.read_csv(
+    "https://gist.github.com/soodoku/f9e18efe98f7d74931d8b4a79a49e6f5/raw/891f0ca1c84fb64c3aa8950f7e21c79b117402bb/state_abbrev_fips.txt",
+    sep=" ",
+    names=["fips", "Abr"],
+)
+# Merging population and fips
+population = population.merge(
+    state_fips, how="left", left_on="state_fips", right_on="fips", indicator=True
+)
+# Only 2 fips weren't merged 0 and 72, values with no interest for us thus we can drop them
+population = population[population["_merge"] == "both"]
+# Keeping just one fips column
+population = population.drop(columns=["_merge", "state_fips"])
+# Merging population and opioids
+opioids = opioids.merge(
+    population,
+    how="left",
+    left_on=["BUYER_STATE", "year"],
+    right_on=["Abr", "year"],
+    indicator=True,
+)
+# All data was matched
+# Dropping columns
+opioids = opioids.drop(columns=["Abr", "_merge"])
+# Importing unemployment
+unemployment = pd.read_excel(
+    "../00_source_data/Additional_datasets/unemployment00-22.xlsx"
+)
+# Taking only unmeployment
+unemp_columns = [name for name in unemployment.columns if "Unemployment_rate" in name]
+vars = list(["State"])
+vars.extend(unemp_columns)
+# Filtering data
+unemployment = unemployment[vars]
+# Melting
+unemployment = pd.melt(unemployment, id_vars=["State"], value_vars=unemp_columns)
+# Collapsing
+unemployment = unemployment.groupby(["State", "variable"], as_index=False).mean()
+# Getting year
+unemployment["year"] = [int(string[-4:]) for string in unemployment["variable"]]
+# drop unnecessary variables
+unemployment.drop(columns=["variable"], inplace=True)
+# renaming
+unemployment = unemployment.rename(columns={"value": "unemployment_rate"})
+print(f"unemployment shape: {unemployment.shape}")
+# merging with opioids
+opioids = opioids.merge(
+    unemployment,
+    left_on=["BUYER_STATE", "year"],
+    right_on=["State", "year"],
+    indicator=True,
+)
+print(f"opioids shape: {opioids.shape}")
+# All values were merged, thus we can drop _merge
+opioids.drop(columns=["_merge", "BUYER_STATE"], inplace=True)
+# Exporting
+opioids.to_csv("../20_intermediate_files/opioids_unemp_educ.csv", index=False)
---
+++ 10_code/50_1_clustering.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("../20_intermediate_files/opioids_unemp_educ.csv")
+# Computing morphine mg bought per capita
+data["morphine_mg_pp"] = data["total_morphine_mg"] / data["tot_pop"]
+# Computing percentage of population with more than 45 years old
+data["pct_greater45"] = data["p_greater45"] / data["tot_pop"]
+# We are going to compute three changes 2006-2007 (TX),2006-2010 (FL), 2006-2012 (WA)
+variables_of_interest = [
+    "State",
+    "unemployment_rate",
+    "morphine_mg_pp",
+    "pct_greater45",
+]
+# 2006 is our oldest year, so every comparison should be considering that year
+year = 2006
+aux = data[data["year"] == year].copy()
+aux = aux[variables_of_interest]
+aux.rename(
+    columns={
+        "unemployment_rate": "unemp_rate_change_since_" + str(year),
+        "morphine_mg_pp": "morphine_mg_pp_change_since_" + str(year),
+        "pct_greater45": "pct_greater45_change_since_" + str(year),
+    },
+    inplace=True,
+)
+data = data.merge(aux, on="State", how="left")
+# Computing the changes
+data["unemp_rate_change_since_" + str(year)] = (
+    data["unemployment_rate"] / data["unemp_rate_change_since_" + str(year)]
+) - 1
+data["morphine_mg_pp_change_since_" + str(year)] = (
+    data["morphine_mg_pp"] / data["morphine_mg_pp_change_since_" + str(year)]
+) - 1
+data["pct_greater45_change_since_" + str(year)] = (
+    data["pct_greater45"] / data["pct_greater45_change_since_" + str(year)]
+) - 1
+
+# Finding the three states for Florida
+variables_of_interest = [
+    "State",
+    "year",
+    "morphine_mg_pp",
+    "unemp_rate_change_since_" + str(year),
+    "morphine_mg_pp_change_since_" + str(year),
+    "pct_greater45_change_since_" + str(year),
+]
+
+######### GETTING THE THREE STATES FOR EACH STATE ##########
+
+# states of interest
+states = {"TX": 2007, "FL": 2010, "WA": 2012}
+final_variables = variables_of_interest + ["distance", "pairing_state"]
+# Empty dataframe
+results = pd.DataFrame(columns=final_variables)
+
+for key in states.keys():
+    state_of_interest = data[data["year"] == states[key]][variables_of_interest].copy()
+    # standarizing the intake of morphine per capita
+    state_of_interest["morphine_mg_pp"] = (
+        state_of_interest["morphine_mg_pp"] - state_of_interest["morphine_mg_pp"].mean()
+    ) / state_of_interest["morphine_mg_pp"].std()
+    # computing fixed values to compute the distance
+    unemp_st = state_of_interest[state_of_interest["State"] == key][
+        "unemp_rate_change_since_" + str(year)
+    ].values[0]
+    morphine_st = state_of_interest[state_of_interest["State"] == key][
+        "morphine_mg_pp_change_since_" + str(year)
+    ].values[0]
+    pct45_st = state_of_interest[state_of_interest["State"] == key][
+        "pct_greater45_change_since_" + str(year)
+    ].values[0]
+    morphine_st_pp = state_of_interest[state_of_interest["State"] == key][
+        "morphine_mg_pp"
+    ].values[0]
+    # Computing the euclidean distance
+    state_of_interest["distance"] = np.sqrt(
+        (state_of_interest["unemp_rate_change_since_" + str(year)] - unemp_st) ** 2
+        + (state_of_interest["morphine_mg_pp_change_since_" + str(year)] - morphine_st)
+        ** 2
+        + (state_of_interest["pct_greater45_change_since_" + str(year)] - pct45_st) ** 2
+        + (state_of_interest["morphine_mg_pp"] - morphine_st_pp) ** 2
+    )
+
+    # Sorting the values
+    state_of_interest.sort_values(by="distance", inplace=True)
+
+    # Selecting the top 3
+    state_of_interest = state_of_interest.iloc[1:4, :]
+    state_of_interest["pairing_state"] = key
+    results = pd.concat([results, state_of_interest])
+
+results.to_csv(
+    "../20_intermediate_files/counterfactuals_by_euclidean_distance.csv", index=False
+)
